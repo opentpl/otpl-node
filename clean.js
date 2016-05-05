12f8298d28eca15e7e68470b26b256c8e63cec71
Jun Hwong <jun@diosay.com>
2016-05-05 19:05:42 +0800

    为发布到npm而做的一些优化

---
+++ clean.js
@@ -0,0 +1,60 @@
+var fs = require('fs')
+
+var root = __dirname + '/dst'
+
+function unlink(file) {
+    
+    console.log('delete file:',file)
+    
+    var stat;
+    try {
+        stat = fs.statSync(file)
+    }
+    catch (err) {
+        console.log(err)
+        return
+    }
+    
+    if (stat.isDirectory()) {
+        try {
+            var files = fs.readdirSync(file)
+            for (var i in files) {
+                unlink(file + '/' + files[i])
+            }
+            try {
+                fs.rmdirSync(file)
+            }
+            catch (err) {
+                console.error(err)
+            }
+        }
+        catch (err) {
+            console.error(err)
+        }
+
+    }
+    else if (stat.isFile()) {
+        try {
+            fs.unlinkSync(file)
+        }
+        catch (err) {
+            console.error(err)
+        }
+    }
+
+}
+
+console.log('clean directory:',root)
+try {
+    var files = fs.readdirSync(root)
+    
+    for (var i in files) {
+        
+        unlink(root + '/' + files[i])
+    }
+    
+}
+catch (err) {
+    //ignored
+}
+console.log('clean completed!')
