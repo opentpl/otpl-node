47604c53985cb5e0ac285148a65981c0b99e86f2
Jun Hwong <jun@diosay.com>
2016-05-14 18:37:59 +0800

    更改内置函数定义文件名

--- src/opcodes.ts
+++ src/opcodes.ts
@@ -6,7 +6,7 @@ import * as utils from './utils';
 import Loader from './loader';
 import Writer from './writer';
 import Context from './context';
-import GetFunc from './builtin-funcs';
+import GetFunc from './builtin-func';
 
 export enum DataType {
     Null = 0x0000,
